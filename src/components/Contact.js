f2ba8ef2a2dcd4d29af23754328be55bb9251dba
Gagan Bansal <bansalgagan2004@gmail.com>
2024-07-10 00:04:05 +0530

    "Update Resume"

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import Resume from '../Data/Gagan1.pdf'
 
 const Contact = () => {
 
@@ -22,17 +23,24 @@ const Contact = () => {
         window.location.href = "https://twitter.com/bansalgagan2004";
     }
 
+    const downloadResume = () => {
+        const link = document.createElement('a');
+        link.href = Resume;
+        link.download = 'GaganCV.pdf';
+        link.click();
+    };
+
     return (
         <div>
             <div className='flex flex-row justify-center items-center gap-3 mt-3'>
-                <button className=' px-4 py-2 text-white rounded-lg font-bold shadow-md transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-blue-700 duration-300'>Download CV</button>
+                <button onClick={downloadResume} className=' px-4 py-2 text-white rounded-lg font-bold shadow-md transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-blue-700 duration-300'>Download CV</button>
                 <button className={`font-bold bg-slate-200 rounded-lg  shadow-md  ${contact ? 'border-4 border-sky-600 text-sky-600 bg-white px-4 py-1' : 'px-4 py-2 '}  `} onClick={ContactHandler}>Contact Me</button>
             </div>
 
             {
                 contact ? (
                     <div className='flex flex-col items-center justify-center mt-5 font-semibold text-white opacity-55'>
-                        <p>Ph: +91 99968 92070</p>
+                        <p>Ph: +91 99968 XXXXX</p>
                         <p>Email: bansalgagan2004@gmail.com</p>
 
 
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,12 +3,14 @@ import React from 'react'
 const Experience = () => {
     return (
         <div>
-            <div className='flex flex-col sm:flex-col justify-start max-w-550 px-7 text-white md:w-6/12 md:items-center'>
-                <div className='bg-slate-800 p-2 rounded-lg shadow-xl'>
-                    <div>
-                        <p className='font-bold'>ACM CUIET, <span className='text-red-500 font-normal'>Sep 23 - present</span></p>
-                        <p className='text-slate-300'>Web Team</p>
-                        <p className='text-slate-300'>Building Website for ACM Club</p>
+            <div className='flex flex-col justify-center items-center max-w-550 px-7 text-white gap-3 '>
+                <div className='sm:flex sm:flex-row sm:w-6/12 sm:items-center sm:justify-center'>
+                    <div className='bg-slate-800 p-2 rounded-lg shadow-xl w-fit'>
+                        <div>
+                            <p className='font-bold'>ACM CUIET, <span className='text-red-500 font-normal'>Sep 23 - present</span></p>
+                            <p className='text-slate-300'>Web Team</p>
+                            <p className='text-slate-300'>Building Website for ACM Club</p>
+                        </div>
                     </div>
                 </div>
             </div>
